6d342661445702d0d51041819a52574a3394d243
Joey Nevarez <josephnevarez12@gmail.com>
2020-07-23 09:33:22 -0700

    cleans up day 3 code

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -5,67 +5,68 @@ class CPU:
     def __init__(self):
         """Construct a new CPU."""
         self.ram = [0] * 256
-        self.reg = [0] * 8
-        self.SP = 7
-        self.reg[self.SP] = 0xF4
+        self.registers = [0] * 8
+        self.registers[7] = 0xF4
+        # self.SP = 7
         self.pc = 0
         self.branchtable = {
-            0b10000010: self.handle_LDI,
-            0b01000111: self.handle_PRN,
-            0b00000001: self.handle_HLT,
-            0b10100010: self.handle_MUL,
-            0b01000110: self.handle_POP,
-            0b01000101: self.handle_PUSH,
+            0b10000010: self.LDI,
+            0b01000111: self.PRN,
+            0b00000001: self.HLT,
+            0b10100010: self.MUL,
+            0b01000110: self.POP,
+            0b01000101: self.PUSH,
         }
 
 
-    def handle_POP(self, op_a, op_b):
+    def POP(self, op_a, op_b):
         # "Pop the value at the top of the stack into the given register."
             # 1. Copy the value from the address pointed to by `SP` to the given register.
             # 2. Increment `SP`.
 
-        # get register number for the binary code after pop instruction 
-        reg_num = op_a
-     
-        key_in_stack = self.reg[self.SP]
-        self.reg[reg_num] = self.ram[key_in_stack]
 
-        self.reg[self.SP] += 1
+        # get the stack pointer (where do we look?)
+        sp = self.registers[7]
+
+        # # get register number for the binary code after pop instruction, to put value in later
+        reg_num = op_a
+
+        # use stack pointer to get the value
+        value = self.ram[sp]
+
+        # put the value into the given register
+        self.registers[reg_num] = value
+
+        self.registers[7] += 1
         self.pc += 2
 
-    def handle_PUSH(self, op_a, op_b):
+    def PUSH(self, op_a, op_b):
         # decrement SP
         # F3
-        self.reg[self.SP] -= 1   
-       
-        # get the value we want to store
-        #  reg_num = self.ram[self.pc + 1] == 0000000 ??????
-        # reg_num = op_a
-        # value = self.reg[00000000] = 1
-        # value = self.reg[reg_num]
-        value = self.reg[op_a]
-        # figure out where to store it
-        # top_of_stack_addr = self.reg[self.SP] = F3
-        # top_of_stack_addr = self.reg[self.SP]
-        # store it
-        # self.ram[top_of_stack_addr] = value | self.ram[f3] = 1
-        # self.ram_write(value, top_of_stack_addr)
-        self.ram_write(value, self.reg[self.SP])
+        self.registers[7] -= 1
+
+        reg_num = op_a
+
+        value = self.registers[reg_num]
+        sp = self.registers[7]
+
+        self.ram[sp] = value
+        
         self.pc += 2
 
-    def handle_LDI(self, op_a, op_b):
-        self.reg[op_a] = op_b
+    def LDI(self, op_a, op_b):
+        self.registers[op_a] = op_b
         self.pc +=3
 
-    def handle_PRN(self, op_a, op_b):
-        print(self.reg[op_a])
+    def PRN(self, op_a, op_b):
+        print(self.registers[op_a])
         self.pc +=2
 
-    def handle_HLT(self, op_a, op_b):
+    def HLT(self, op_a, op_b):
         exit()
 
-    def handle_MUL(self, op_a, op_b):
-        self.reg[op_a] = self.reg[op_a] * self.reg[op_b]
+    def MUL(self, op_a, op_b):
+        self.registers[op_a] = self.registers[op_a] * self.registers[op_b]
         self.pc +=3
 
     def load(self):
@@ -91,7 +92,7 @@ class CPU:
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
         if op == "ADD":
-            self.reg[reg_a] += self.reg[reg_b]
+            self.registers[reg_a] += self.registers[reg_b]
         #elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
@@ -111,7 +112,7 @@ class CPU:
         ), end='')
 
         for i in range(8):
-            print(" %02X" % self.reg[i], end='')
+            print(" %02X" % self.registers[i], end='')
 
         print()
 
